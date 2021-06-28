b6fb42f3f70989864232e29b42c7b5df78170000
Eyoatam <eyoatamtamirat7@gmail.com>
2021-06-28 20:25:53 +0300

    upgrade deno_std to 0.99.0 pt.3

--- euclidean-algorithm/euclidean_algorithm_test.ts
+++ euclidean-algorithm/euclidean_algorithm_test.ts
@@ -1,4 +1,4 @@
-import { assertStrictEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts";
+import { assertStrictEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
 import { euclideanAlgorithm } from "./euclidean_algorithm.ts";
 
 Deno.test("GCD", () => {
